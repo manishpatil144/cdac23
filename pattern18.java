b616af205dbb418614c67149d8118a769d75e0ee
manishpatil144 <128138513+manishpatil144@users.noreply.github.com>
2023-03-18 16:50:10 +0530

    Add files via upload

---
+++ pattern18.java
@@ -0,0 +1,12 @@
+class pattern18
+{
+	public static void main(String args[]){
+		int alpha=65;
+		for(int i=5;i>=0;i--){
+			for(int j=0;j<=i;j++){
+				System.out.print((char)(alpha+j)+" ");
+			}
+			System.out.println();
+		}
+	}
+}
