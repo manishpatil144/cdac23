571d85d4f9747561838f36cfba5f39bd67d57375
manishpatil144 <128138513+manishpatil144@users.noreply.github.com>
2023-03-21 08:07:34 +0530

    Add files via upload

---
+++ starpattern6.java
@@ -0,0 +1,16 @@
+class starpattern6{
+	public static void main(String args[]){
+		for(int i=1;i<=5;i++){
+			for(int k=1;k<=i;k++){
+				System.out.print(" ");
+			}
+			for(int j=5;j>=i;j--){
+				System.out.print("*");
+			}
+			for(int r=4;r>=i;r--){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		}
+}
---
+++ starpattern7.java
@@ -0,0 +1,30 @@
+class starpattern7{
+	public static void main(String args[]){
+		for(int i=1;i<=5;i++){
+			for(int k=4;k>=i;k--){
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++){
+				System.out.print("*");
+			}
+			for(int r=2;r<=i;r++){
+				System.out.print("*");
+			}
+			System.out.println();
+		
+		}
+		
+		for(int i=1;i<=4;i++){
+			for(int k=1;k<=i;k++){
+				System.out.print(" ");
+			}
+			for(int j=4;j>=i;j--){
+				System.out.print("*");
+			}
+			for(int r=3;r>=i;r--){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
